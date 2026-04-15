75f08a0021d2e879106c9e694a33562394cbf4a8
Ryan Grant <114532622+ryan-grant25@users.noreply.github.com>
2026-04-15 09:56:27 -0400

    Create drinks.php

---
+++ app/api/drinks.php
@@ -0,0 +1,8 @@
+<?php
+include '../db_connect.php';
+include 'BaseController.php';
+header('Content-Type: application/json');
+
+$fields = ['name', 'description', 'recipe', 'base_spirit_ID', 'flavor_profile', 'glassware_type', 'difficulty', 'category', 'created_by_userID'];
+$controller = new BaseController($conn, "Drink", "DrinkID", $fields);
+$controller->handleRequest($_SERVER['REQUEST_METHOD']);
