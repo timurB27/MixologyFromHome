821479eb9027e9333116a816f5dc0edc18b2655e
Ryan Grant <114532622+ryan-grant25@users.noreply.github.com>
2026-04-15 10:53:12 -0400

    Update shopping_items.php

--- app/api/shopping_items.php
+++ app/api/shopping_items.php
@@ -1,6 +1,6 @@
 <?php
-include '../db_connect.php';
-include 'BaseController.php';
+include __DIR__ . '/../db_connect.php';
+include __DIR__ . '/BaseController.php';
 header('Content-Type: application/json');
 
 $fields = ['ShoppinglistID', 'IngredientID', 'Unit', 'Is_purchased', 'notes'];
