11d8f268f55549b7869442b5a616075f5f045639
Ryan Grant <114532622+ryan-grant25@users.noreply.github.com>
2026-04-15 10:52:14 -0400

    Update drinks.php

--- app/api/drinks.php
+++ app/api/drinks.php
@@ -1,6 +1,6 @@
 <?php
-include '../db_connect.php';
-include 'BaseController.php';
+include __DIR__ . '/../db_connect.php';
+include __DIR__ . '/BaseController.php';
 header('Content-Type: application/json');
 
 $fields = ['name', 'description', 'recipe', 'base_spirit_ID', 'flavor_profile', 'glassware_type', 'difficulty', 'category', 'created_by_userID'];
